0345cad8d6e1b2306ca0da3dcaf2c766d48c024b
Кристина Гуляева <kristinagul.24@gmail.com>
2022-10-13 08:53:07 +1000

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1_Пр_5_Гуляева_УП_02._02
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
